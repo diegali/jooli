fcda53b8105f9871b9706217fffbb0d0338283dd
diegali <154539043+diegali@users.noreply.github.com>
2026-03-05 12:48:01 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+import { initAuth } from "./auth.js";
+import { initEvents } from "./events.js";
+import { initCalendar } from "./calendar.js";
+import { initUI } from "./ui.js";
+
+const loginDiv = document.getElementById("loginDiv");
+const appDiv = document.getElementById("appDiv");
+
+document.getElementById("darkBtn").onclick =
+  ()=>document.body.classList.toggle("dark");
+
+initAuth(()=>{
+
+  loginDiv.style.display="none";
+  appDiv.style.display="block";
+
+  initUI();
+  initEvents();
+  initCalendar();
+
+});
+
+if("serviceWorker" in navigator){
+  navigator.serviceWorker.register("./sw.js")
+  .then(()=>console.log("Service Worker activo"))
+  .catch(err=>console.log("SW error",err));
+}
