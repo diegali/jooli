c183d284223ec1da87c5adf34ad771176211f7f9
Diego Galindo <die.galindo@gmail.com>
2026-03-05 14:29:38 -0300

    Funcionando

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { initAuth } from "./auth.js";
 import { initEvents } from "./events.js";
 import { initCalendar } from "./calendar.js";
@@ -25,32 +24,4 @@ if("serviceWorker" in navigator){
   navigator.serviceWorker.register("./sw.js")
   .then(()=>console.log("Service Worker activo"))
   .catch(err=>console.log("SW error",err));
-=======
-import { initAuth } from "./auth.js";
-import { initEvents } from "./events.js";
-import { initCalendar } from "./calendar.js";
-import { initUI } from "./ui.js";
-
-const loginDiv = document.getElementById("loginDiv");
-const appDiv = document.getElementById("appDiv");
-
-document.getElementById("darkBtn").onclick =
-  ()=>document.body.classList.toggle("dark");
-
-initAuth(()=>{
-
-  loginDiv.style.display="none";
-  appDiv.style.display="block";
-
-  initUI();
-  initEvents();
-  initCalendar();
-
-});
-
-if("serviceWorker" in navigator){
-  navigator.serviceWorker.register("./sw.js")
-  .then(()=>console.log("Service Worker activo"))
-  .catch(err=>console.log("SW error",err));
->>>>>>> fcda53b8105f9871b9706217fffbb0d0338283dd
 }
--- sw.js
+++ sw.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const CACHE_NAME = "jooli-agenda-v1";
 
 const urlsToCache = [
@@ -24,31 +23,4 @@ self.addEventListener("fetch", event => {
     caches.match(event.request)
     .then(response => response || fetch(event.request))
   );
-=======
-const CACHE_NAME = "jooli-agenda-v1";
-
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/css/style.css",
-  "/js/main.js",
-  "/js/auth.js",
-  "/js/events.js",
-  "/js/calendar.js",
-  "/js/ui.js"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-    .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-    .then(response => response || fetch(event.request))
-  );
->>>>>>> fcda53b8105f9871b9706217fffbb0d0338283dd
 });
